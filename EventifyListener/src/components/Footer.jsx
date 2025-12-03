1c8b4c79a1be67f0ca62062063bd5f6d94428825
Emilien <emilien.derobert@gmail.com>
2025-12-03 11:10:22 +0100

    fix: Linkedin de Jordan

--- EventifyListener/src/components/Footer.jsx
+++ EventifyListener/src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
                 <p>©
                     <a href="https://www.linkedin.com/in/drdb-emilien/">Emilien de Robert</a> -
                     <a href="https://www.linkedin.com/in/amaury-celarier/">Amaury Celarier</a> -
-                    <a href="https://www.linkedin.com/in/jordan-dupuy/">Jordan Dupuy</a> -
+                    <a href="https://www.linkedin.com/in/jordan-dpy/">Jordan Dupuy</a> -
                     <a href="https://www.linkedin.com/in/nathanael-seitz/">Nathanaël Seitz</a> | 2025
                 </p>
                 <a href="http://localhost:8000/api/docs">accedez a la documentation de l'API</a>
